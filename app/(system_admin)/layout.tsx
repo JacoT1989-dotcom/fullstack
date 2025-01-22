6fc0a50f956cea634cc5080d7cec6867d1954f2c
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-22 19:25:26 +0200

    set

--- app/(system_admin)/layout.tsx
+++ app/(system_admin)/layout.tsx
@@ -1,9 +1,9 @@
 import { validateRequest } from "@/auth";
-import { redirect } from "next/navigation";
-import Navbar from "./_components/Navbar";
-import { Toaster } from "sonner";
-import SidebarNavigation from "./_components/SlideInSideBar";
 import SessionProvider from "./SessionProvider";
+import Navbar from "./_components/Navbar";
+import SidebarNavigation from "./_components/SlideInSideBar";
+import { redirect } from "next/navigation";
+import { Toaster } from "sonner";
 
 export const dynamic = "force-dynamic";
 
