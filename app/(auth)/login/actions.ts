87b4dc7cc253bf9d25f562cac77d9d4082e7b501
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-26 15:39:47 +0200

    set comments

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -57,12 +57,14 @@ export async function login(
       };
     }
 
+    ////////////////////// THIS IS THE PART WHERE THE FUNCTION IS IN A POSITIVE STATE//////////
+
     // Create session in the database
     const dbSession = await prisma.session.create({
       data: {
         id: crypto.randomUUID(),
         userId: existingUser.id,
-        expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+        expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days, 24 HOURS, 60 SEC, 60 MIN, 1000 MIL SEC
       },
     });
 
