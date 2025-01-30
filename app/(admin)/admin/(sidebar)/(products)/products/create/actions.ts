641951b340928a7e7269422664b315a9ece2d0f1
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-30 17:19:05 +0200

    SET

--- app/(admin)/admin/(sidebar)/(products)/products/create/actions.ts
+++ app/(admin)/admin/(sidebar)/(products)/products/create/actions.ts
@@ -1,25 +1,19 @@
 "use server";
 
 import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
 
 export async function createProduct() {
   try {
     const { user } = await validateRequest();
-    if (!user) {
-      throw new Error("Unauthorized access");
+    if (!user) throw new Error("Unauthorized access");
+    if (user.role !== "ADMIN" && user.role !== "SUPERADMIN") {
+      return redirect("/login");
     }
   } catch (error) {
     console.error("Full error details:", {
       message: error instanceof Error ? error.message : "Unknown error",
       error,
     });
-
-    return {
-      success: false,
-      error:
-        error instanceof Error
-          ? error.message
-          : "An unexpected error occurred while creating the product",
-    };
   }
 }
