09ceb01f6735eca3d1b33df899798fdfcecd9057
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-01-31 13:37:48 +0200

    set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import { Toaster } from "sonner";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
