b48dc09d2de7b344c06b2232911b3c19041a618b
JacoT1989-dotcom <jacothiart52@gmail.com>
2025-02-03 11:43:38 +0200

    set

--- app/(public)/all-in-headwear/actions.ts
+++ app/(public)/all-in-headwear/actions.ts
@@ -21,7 +21,7 @@ export async function getHeadwearProducts(): Promise<ProductActionResult> {
         sellingPrice: true,
       },
       orderBy: {
-        createdAt: "desc",
+        createdAt: "asc",
       },
     });
 
